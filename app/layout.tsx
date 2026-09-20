a02bb76247f691db43f3ae2ca6bc44a2bbf2ef1e
mlj00828 <mljefferson2008@gmail.com>
2026-09-20 02:03:11 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [{ url: "/icons/apple-icon.png", sizes: "180x180" }],
   },
